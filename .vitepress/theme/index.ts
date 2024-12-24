d06268030eab7aaec709ecc8805c29e6422d2fd4
lk <c1824503917@outlook.com>
2024-12-24 23:42:55 +0800

    图片缩放

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,8 +11,28 @@ import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 import type { Theme as ThemeConfig } from 'vitepress'
 import { h } from 'vue'
 import '/public/style/my.css'
+
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+
 export const Theme: ThemeConfig = {
   extends: DefaultTheme,
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      // mediumZoom(".main img", { background: "var(--vp-c-bg)" }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+      mediumZoom(".main img")
+    };
+    onMounted(() => {
+      initZoom(); 
+    }); 
+    watch(  
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // 其他的配置...
